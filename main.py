4f88941ffa122e5f5432b38636517c5f65c0d0cb
DosSantos2077 <205238080+DosSantos2077@users.noreply.github.com>
2025-06-01 14:23:20 -0300

    primeira janela1.1

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import tkinter as tk
 
-def main():
-    
-    janela = tk.Tk()
-    janela.title("Minha Janela Tkinter")  
-    janela.geometry("600x500")  
-    
-    janela.mainloop()
 
-if __name__ == "__main__":
-    main()
+root = tk.Tk()
+root.title("Menu")  
+root.geometry("600x500")  
+    
+root.mainloop()
