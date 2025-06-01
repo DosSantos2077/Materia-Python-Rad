b54b5adad49f6b4e94df012e34209d17509f0b08
DosSantos2077 <205238080+DosSantos2077@users.noreply.github.com>
2025-06-01 16:01:04 -0300

    Buttons

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("Menu")  
-root.geometry("600x500")  
-    
+root.geometry("300x200")  
+root.configure(bg="lightblue")
+
+btn1 = tk.Button(root, text="Botão 1")
+btn2 = tk.Button(root, text="Botão 2")
+btn3 = tk.Button(root, text="Botão 3")
+
+btn1.pack(fill="both", expand=True,padx=10, pady=5)
+btn2.pack(fill="both", expand=True,padx=10, pady=5)
+btn3.pack(fill="both", expand=True,padx=10, pady=5)
+
 root.mainloop()
